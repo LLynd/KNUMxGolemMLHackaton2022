9a4dbec08c8c306442bfc3c61174562a6178efc5
vnika <vskibicka@gmail.com>
2022-06-05 02:50:39 +0200

    secret_data

--- src/utils/load_data_secret.py
+++ src/utils/load_data_secret.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import numpy as np
 
+
 def load_data(mode):
     if mode == 'TRAIN':
         json_path = '../../data/reference_images_part1.json'
@@ -16,32 +17,33 @@ def load_data(mode):
     images = pd.DataFrame(data['images'])
     annotations = pd.DataFrame(data['annotations'])
     categories = pd.DataFrame(data['categories'])
-
+    
     df = pd.DataFrame()
-
-
+    
+    
     X = []
     y = []
     y_desc = []
     occluded = []
     bboxes = []
     im_ids = []
-
+    
     for instance in data['annotations']:
         im_id = instance['image_id']
         bbox = instance['bbox']
         y.append(instance['category_id'])
         bboxes.append(np.asarray(bbox).astype('int64'))
-        im_ids.append(im_id)
+        #print(images.loc[images['id']==im_id]['file_name'])
+        im_ids.append(images.loc[images['id']==im_id]['file_name'].values[0])
         y_desc.append(categories.loc[categories['id']==instance['category_id']]['name'].values[0])
         if mode=='TRAIN':
             occluded.append(False)
         elif mode=='VAL':
             occluded.append(instance['occluded'])
-
+        
     df['bbox'] = bboxes
     df['y'] = y
     df['desc'] = y_desc
     df['im_id'] = im_ids
-
+    
     return df
